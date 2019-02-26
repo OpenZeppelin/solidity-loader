6cd7a995a40b1876f93fdf962a0ccd183e61650b
Igor Yalovoy <igor@yalovoi.net>
2019-02-26 22:15:06 +0300

    Add disable flag for config

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ module.exports = async function loader(source) {
   try {
     const options = getOptions(this);
     const network = (options && options.network) ? options.network : 'development';
+    const disabled = options && options.disabled;
     const contractPath = this.context;
     const cwd = path.resolve(contractPath, '..');
     const contractFilePath = this.resourcePath;
@@ -56,19 +57,22 @@ module.exports = async function loader(source) {
     const compiledContractPath = path
       .resolve(config.contracts_build_directory, `${contractName}.json`);
 
-    // wait until compile/push/update is done
-    while (isZeppelinBusy) await wait(500);
+    // if loader is disabled do not compile/push/upgrade, but still serve .json contracts from file system.
+    if (!disabled) {
+      // wait until compile/push/update is done
+      while (isZeppelinBusy) await wait(500);
 
-    isZeppelinBusy = true;
+      isZeppelinBusy = true;
 
-    try {
-      // push new code into local blockchain
-      let result = await exec(`zos push --network ${network}`, { cwd });
-      // update proxy contract
-      result = await exec(`zos update ${contractName} --network ${network}`, { cwd });
-    } finally {
-      // release lock
-      isZeppelinBusy = false;
+      try {
+        // push new code into local blockchain
+        let result = await exec(`zos push --network ${network}`, { cwd });
+        // update proxy contract
+        result = await exec(`zos update ${contractName} --network ${network}`, { cwd });
+      } finally {
+        // release lock
+        isZeppelinBusy = false;
+      }
     }
 
     // read JSON contract produced by compile and return it
