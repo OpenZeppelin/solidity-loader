8feced71fa02165f848f49c19ddc1b4c37e7ff25
Igor Yalovoy <igor@yalovoi.net>
2019-03-02 00:27:52 +0300

    Allow to specify contract name; Update child on parent change; * Allow to specify contract name for .sol files, like require("Contract.sol?contract=Counter") * Track dependencies for contracts, so children are updated when parents are modified.

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const path = require('path');
-const { getOptions } = require('loader-utils');
+
+const pathIsInside = require('path-is-inside');
+
+const { getOptions, parseQuery } = require('loader-utils');
 
 const { exec, readFile, wait } = require('./lib/util');
-const { getConfig } = require('./lib/truffle');
+const { getConfig, getImports } = require('./lib/truffle');
 
 // Lock to prevent race conditions
 let isZeppelinBusy = false;
 
 module.exports = async function loader(source) {
   const callback = this.async();
+  const addDependency = this.addDependency;
 
   try {
+    const params = parseQuery(this.resourceQuery || '?');
     const options = getOptions(this);
     const network = (options && options.network) ? options.network : 'development';
     const disabled = options && options.disabled;
-    const contractPath = this.context;
-    const cwd = path.resolve(contractPath, '..');
+    const contractFolderPath = this.context;
+    const cwd = path.resolve(contractFolderPath, '..');
     const contractFilePath = this.resourcePath;
     const config = await getConfig({ network, cwd });
     const contractsBuildDirectory = config.contracts_build_directory;
     const contractFileName = path.basename(contractFilePath);
-    const contractName = contractFileName.charAt(0).toUpperCase() + contractFileName.slice(1, contractFileName.length - 4);
+    const contractName = params.contract || contractFileName.charAt(0).toUpperCase() + contractFileName.slice(1, contractFileName.length - 4);
     const compiledContractPath = path
-      .resolve(config.contracts_build_directory, `${contractName}.json`);
+      .resolve(contractsBuildDirectory, `${contractName}.json`);
 
     // if loader is disabled do not compile/push/upgrade, but still serve .json contracts from file system.
     if (!disabled) {
@@ -44,9 +49,15 @@ module.exports = async function loader(source) {
 
     // read JSON contract produced by compile and return it
     const solJSON = await readFile(compiledContractPath, 'utf8');
+    // get all contract imports
+    const imports = getImports(JSON.parse(solJSON));
+    // imports located inside our contracts folder are the only one we have to watch
+    const projectImports = imports.filter(imp => pathIsInside(imp, contractFolderPath));
+    // add these imports as dependencies for a contract
+    projectImports.map(imp => addDependency(imp));
     callback(null, solJSON);
   } catch (e) {
     // report error here, because configuration seems to be lacking
-    callback(e, {});
+    callback(e, '{}');
   }
 };
--- test/loader.test.js
+++ test/loader.test.js
@@ -21,27 +21,40 @@ const disabledOptions = {
   disabled: true,
 };
 
-const contractName = 'Contract';
+const contractFilePath = './data/contracts/Contract.sol';
 const cwd = { cwd: path.resolve(__dirname, 'data') };
 
-const execute = async (options) => {
-  const stats = await compiler('./data/contracts/contract.sol', options);
+util.exec = jest.fn();
+
+const execute = async (options, source, contractName) => {
+  const stats = await compiler(source, options);
   const output = stats.toJson().modules[0].source;
 
   const contract = requireFromString(output);
 
-  expect(contract.contractName).toBe('Counter');
+  expect(contract.contractName).toBe(contractName);
   expect(contract.abi.length).toBeGreaterThan(0);
   expect(Object.keys(contract.networks).length).toBeGreaterThan(0);
 };
 
 beforeEach(() => {
-  util.exec = jest.fn();
+  jest.resetAllMocks();
 });
 
 test('Runs truffle compile, zos push, and zos update commands to produce fresh .json files', async (done) => {
+  const contractName = 'Contract';
   const { network } = defaultOptions;
-  await execute(defaultOptions);
+  await execute(defaultOptions, contractFilePath, contractName);
+  expect(util.exec).toHaveBeenCalledTimes(2);
+  expect(util.exec).toHaveBeenCalledWith(`zos update ${contractName} --network ${network}`, cwd);
+  expect(util.exec).toHaveBeenCalledWith(`zos push --network ${network}`, cwd);
+  done();
+});
+
+test('Runs truffle compile, zos push, and zos update commands to produce fresh .json files with contract not being the same as filename', async (done) => {
+  const { network } = defaultOptions;
+  const contractName = 'Counter';
+  await execute(defaultOptions, `${contractFilePath}?contract=${contractName}`, contractName);
   expect(util.exec).toHaveBeenCalledTimes(2);
   expect(util.exec).toHaveBeenCalledWith(`zos update ${contractName} --network ${network}`, cwd);
   expect(util.exec).toHaveBeenCalledWith(`zos push --network ${network}`, cwd);
@@ -49,7 +62,8 @@ test('Runs truffle compile, zos push, and zos update commands to produce fresh .
 });
 
 test('Serves json files from file system while disabled', async (done) => {
-  await execute(disabledOptions);
+  const contractName = 'Contract';
+  await execute(disabledOptions, contractFilePath, contractName);
   expect(util.exec).toHaveBeenCalledTimes(0);
   done();
 });
